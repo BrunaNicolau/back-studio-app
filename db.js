38fbb920ecf6d120b7a8f14c1ec85dd0d0997243
Bruna Nicolau <brunanicolauolivera@gmail.com>
2022-11-05 11:46:33 -0300

    conectando com banco

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/back-studio-app');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/back-studio-app');
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String
-}, { collection: 'users' }
-);
+// const userSchema = new mongoose.Schema({
+//   username: String,
+//   email: String
+// }, { collection: 'users' }
+// );
 
-module.exports = { Mongoose: mongoose, UserSchema: userSchema }
+// module.exports = { Mongoose: mongoose, UserSchema: userSchema }
+
+const mongoClient = require('mongodb').MongoClient;
+mongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (error, connection) => {
+  if (error) return console.log(error);
+  global.connection = connection.db('studio-app')
+  console.log('conectou')
+});
+
+module.exports = {}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
+// /* GET home page. */
+// router.get('/', async (req, res, next) => {
+//   const db = require("../db");
+//   const Users = db.Mongoose.model('users', db.UserSchema, 'users');
+
+//   const docs = await Users.find({}).lean().exec();
+//   res.render('index', { docs });
+// });
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const db = require('../db')
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  const db = require("../db");
-  const Users = db.Mongoose.model('users', db.UserSchema, 'users');
 
-  const docs = await Users.find({}).lean().exec();
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'express'})
+  const docs = Users.find({}).lean().exec();
   res.render('index', { docs });
-});
+})
 
 module.exports = router;
